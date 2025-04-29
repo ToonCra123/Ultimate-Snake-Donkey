d5297bdcd35672d91d7604648b8c6890067d3736
Mykull <michaelguillory.mg@gmail.com>
2025-04-29 08:32:32 -0700

    shit

--- Assets/src/PlayerMovement.cs
+++ Assets/src/PlayerMovement.cs
@@ -1,24 +1,43 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement2D : MonoBehaviour
 {
-    public float playerSpeed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement Settings")]
+    public float moveSpeed = 8f;
+    public float jumpForce = 12f;
+
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float moveInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        // Handle input
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector3.left * playerSpeed, ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().AddForce(Vector3.right * playerSpeed, ForceMode2D.Impulse);
-        }
+    }
+
+    void FixedUpdate()
+    {
+        // Ground check
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Move
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
     }
 }
