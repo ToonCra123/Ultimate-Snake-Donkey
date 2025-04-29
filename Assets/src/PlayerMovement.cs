ea997812f038555ea79776a951033332a17d3963
Mykull <michaelguillory.mg@gmail.com>
2025-04-29 08:08:28 -0700

    PlayerMoves

--- Assets/src/PlayerMovement.cs
+++ Assets/src/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float playerSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.left * playerSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.right * playerSpeed, ForceMode2D.Impulse);
+        }
     }
 }
